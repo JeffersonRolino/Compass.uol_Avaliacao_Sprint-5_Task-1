ea1547edf7b4ac7c99c515e559ff1da27b22c0ad
JeffersonRolino <jefferson.rolino@gmail.com>
2022-07-26 10:11:08 -0300

    Adicionado DTOs: OrderDTO e ItemDTO

---
+++ src/main/java/com/github/jeffersonrolino/avaliacao_sprint_5_task_1/dtos/ItemDTO.java
@@ -0,0 +1,35 @@
+package com.github.jeffersonrolino.avaliacao_sprint_5_task_1.dtos;
+
+import com.github.jeffersonrolino.avaliacao_sprint_5_task_1.models.Sale;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ItemDTO {
+    private Long id;
+
+    @NotNull @NotEmpty
+    private String name;
+
+    @NotNull
+    private LocalDateTime creationDate;
+
+    @NotNull
+    private LocalDateTime expirationDate;
+
+    @NotNull
+    private double price;
+
+    @NotNull @NotEmpty
+    private String description;
+
+    private List<Sale> sales;
+}
---
+++ src/main/java/com/github/jeffersonrolino/avaliacao_sprint_5_task_1/dtos/OrderDTO.java
@@ -0,0 +1,33 @@
+package com.github.jeffersonrolino.avaliacao_sprint_5_task_1.dtos;
+
+import com.github.jeffersonrolino.avaliacao_sprint_5_task_1.models.Item;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class OrderDTO {
+    private Long id;
+
+    @NotNull
+    private String cpf;
+
+    @NotNull @NotEmpty
+    private List<Item> itens;
+
+    @NotNull @NotEmpty
+    private Double total;
+
+
+    public OrderDTO(String cpf, List<Item> itens, Double total) {
+        this.cpf = cpf;
+        this.itens = itens;
+        this.total = total;
+    }
+}
