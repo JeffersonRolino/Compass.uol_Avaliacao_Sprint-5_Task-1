a73a5ba6ecfe6acbbe2ff1cdbedc130035b525aa
JeffersonRolino <jefferson.rolino@gmail.com>
2022-07-26 09:34:48 -0300

    Adicionada Classe OrderController

---
+++ src/main/java/com/github/jeffersonrolino/avaliacao_sprint_5_task_1/controller/OrderController.java
@@ -0,0 +1,9 @@
+package com.github.jeffersonrolino.avaliacao_sprint_5_task_1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(("/api"))
+public class OrderController {
+}
