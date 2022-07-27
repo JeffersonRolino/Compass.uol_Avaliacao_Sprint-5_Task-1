1586669b62a63de41993a686e09941fac2e13609
JeffersonRolino <jefferson.rolino@gmail.com>
2022-07-26 22:40:48 -0300

    Adicionado classe LocalDateTimeParser no pacote parsers

---
+++ src/main/java/com/github/jeffersonrolino/avaliacao_sprint_5_task_1/parsers/LocalDateTimeParser.java
@@ -0,0 +1,11 @@
+package com.github.jeffersonrolino.avaliacao_sprint_5_task_1.parsers;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeParser {
+    public static LocalDateTime parseAndFormat(String data){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss.sss");
+        return LocalDateTime.parse(data, formatter);
+    }
+}
